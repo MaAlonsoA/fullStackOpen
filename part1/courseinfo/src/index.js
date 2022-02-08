79c82e762eb28dff6d29b01aea49042217bc2b87
Marcos Alonso <ma_lonso94@hotmail.com>
2022-02-08 18:31:29 +0100

    Refactor for 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,29 +6,35 @@ import { Total } from "./components/total";
 import { Content } from "./components/content";
 
 const App = () => {
-	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const course = 'Half Stack application development'
+	const part1 = {
+	  name: 'Fundamentals of React',
+	  exercises: 10
+	}
+	const part2 = {
+	  name: 'Using props to pass data',
+	  exercises: 7
+	}
+	const part3 = {
+	  name: 'State of a component',
+	  exercises: 14
+	}
 
 	return (
 		<>
 			<Header courseName={course} />
 			<Content
-				part1={part1}
-				quantity1={exercises1}
-				part2={part2}
-				quantity2={exercises2}
-				part3={part3}
-				quantity3={exercises3}
+				part1={part1.name}
+				quantity1={part1.exercises}
+				part2={part2.name}
+				quantity2={part2.exercises}
+				part3={part3.name}
+				quantity3={part3.exercises}
 			/>
 			<Total
-				exercises1={exercises1}
-				exercises2={exercises2}
-				exercises3={exercises3}
+				exercises1={part1.exercises}
+				exercises2={part2.exercises}
+				exercises3={part3.exercises}
 			/>
 		</>
 	);
