980d12428c33d3e7c8c0434407483e0351922a6a
Marcos Alonso <ma_lonso94@hotmail.com>
2022-02-09 12:08:52 +0100

    Add 1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -2,18 +2,28 @@ import React from "react";
 
 const Statistics = ({ good, neutral, bad }) => {
 	const total = good + neutral + bad;
-    const average = ((good * 1) + (bad * -1)) / total
-	return (
-		<div>
-			<h1>Statistics</h1>
-			<p>Good: {good}</p>
-			<p>Neutral: {neutral}</p>
-			<p>Bad: {bad}</p>
-			<p>Total: {total}</p>
-			<p>Average: {average} </p>
-			<p>Positive: {good / total}%</p>
-		</div>
-	);
+
+	if (total > 0) {
+		const average = (good * 1 + bad * -1) / total;
+		return (
+			<div>
+				<h1>Statistics</h1>
+				<p>Good: {good}</p>
+				<p>Neutral: {neutral}</p>
+				<p>Bad: {bad}</p>
+				<p>Total: {total}</p>
+				<p>Average: {average} </p>
+				<p>Positive: {good / total}%</p>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<h1>Statistics</h1>
+				<p>No feedback given</p>
+			</div>
+		);
+	}
 };
 
 export default Statistics;
