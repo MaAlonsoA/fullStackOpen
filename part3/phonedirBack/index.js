ac60789b92d8f52829231607414b9f8113dccd43
Marcos Alonso <ma_lonso94@hotmail.com>
2022-02-16 18:46:29 +0100

    3.8

--- part3/phonedirBack/index.js
+++ part3/phonedirBack/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {
