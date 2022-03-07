d93acc7076afc633eb46dcb58696f8782dcf1943
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-07 12:56:05 +0100

    refactor: export server as default

--- part4/index.js
+++ part4/index.js
@@ -2,6 +2,6 @@ import app from './app.js';
 import './database.js';
 import * as logger from './utils/logger.js';
 
-app.listen(app.get('port'), () => {
+export default app.listen(app.get('port'), () => {
   logger.info(`Server running on port ${app.get('port')}`);
 });
