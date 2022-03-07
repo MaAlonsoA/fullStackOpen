9a2c9acc09cd2e6356350426c6661f749ccecd60
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-07 12:15:18 +0100

    feat: add config for a test data base

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 
-const { PORT } = process.env || 3001;
-const { MONGODB_URI } = process.env;
+export const { PORT } = process.env || 3001;
+// eslint-disable-next-line import/no-mutable-exports
+export let { MONGODB_URI } = process.env;
 
-export {
-  PORT,
-  MONGODB_URI,
-};
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+}
