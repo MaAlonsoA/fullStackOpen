feb5dbf83f180a32a4d34970b2a078b36d71a006
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-08 12:32:56 +0100

    test: 4.11 add like property validation

--- part4/tests/blogApi.test.js
+++ part4/tests/blogApi.test.js
@@ -23,44 +23,65 @@ beforeEach(async () => {
 });
 
 describe('blog API', () => {
-  test('GET blogs as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-  });
-
-  test('GET all blogs', async () => {
-    // const response = await api.get('/api/blogs');
-    // const content = response.body.map((elem) => elem);
-    const { contents } = await getAllContent();
-    expect(contents).toHaveLength(blogs.length);
-  });
-
   test('the unique identifier is "id" ', async () => {
     const { contents } = await getAllContent();
     contents.forEach((elem) => {
       expect(elem.id).toBeDefined();
     });
   });
+  describe('GET', () => {
+    test('GET blogs as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+    });
 
-  test('POST a valid blog', async () => {
-    const newBlog = {
-      title: 'Go To Statement Considered Harmful2',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 0,
-    };
-    await api.post('/api/blogs')
-      .send(newBlog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+    test('GET all blogs', async () => {
+      // const response = await api.get('/api/blogs');
+      // const content = response.body.map((elem) => elem);
+      const { contents } = await getAllContent();
+      expect(contents).toHaveLength(blogs.length);
+    });
+  });
+  describe('POST', () => {
+    test('POST a valid blog', async () => {
+      const newBlog = {
+        title: 'Go To Statement Considered Harmful2',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 0,
+      };
+      await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    const { contents, response } = await getAllContent();
+      const { contents } = await getAllContent();
 
-    const titles = contents.map((elem) => elem.title);
-    expect(response.body).toHaveLength(blogs.length + 1);
-    expect(titles).toContain(newBlog.title);
+      const titles = contents.map((elem) => elem.title);
+      expect(contents).toHaveLength(blogs.length + 1);
+      expect(titles).toContain(newBlog.title);
+    });
+
+    test('POST without like property it will default to 0', async () => {
+      const newBlog = {
+        title: 'Go To Statement Considered Harmful2',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      };
+
+      await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+      const { contents } = await getAllContent();
+      expect(contents).toHaveLength(blogs.length + 1);
+      contents.forEach((elem) => {
+        if (elem.title === newBlog.title) expect(elem.likes).toBe(0);
+      });
+    });
   });
 });
 
