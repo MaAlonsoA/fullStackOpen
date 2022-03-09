1c7e02d3f6b302f24d5bfb298259c8e848f0ed77
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-09 23:27:03 +0100

    feat: setup part5

---
+++ part5/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './App.jsx';
+
+ReactDOM.render(
+
+  // eslint-disable-next-line react/jsx-filename-extension
+  <App />,
+  document.getElementById('root'),
+);
