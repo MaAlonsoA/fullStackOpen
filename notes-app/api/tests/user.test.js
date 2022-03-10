6d30a67f4e863853ea7e0ca466cc4c441a52d17d
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-10 19:06:09 +0100

    style

--- notes-app/api/tests/user.test.js
+++ notes-app/api/tests/user.test.js
@@ -22,6 +22,7 @@ describe('GET', () => {
 describe('POST', () => {
   test('POST fresh user', async () => {
     const newUser = { username: 'fresh2', password: '123456', name: 'newnew' };
+
     await api
       .post('/api/users')
       .send(newUser)
@@ -35,10 +36,12 @@ describe('POST', () => {
 
   test('POST fails with proper status code abd nessage if user is already taken', async () => {
     const newUser = initialUsers[0];
+
     const result = await api.post('/api/users')
       .send(newUser)
       .expect(400)
       .expect('Content-Type', /application\/json/);
+
     expect(result.body.error)
       .toContain('User validation failed: username: Error, expected `username` to be unique. Value: `root`');
 
@@ -53,6 +56,7 @@ describe('POST', () => {
       .send(newUser)
       .expect(400)
       .expect('Content-Type', /application\/json/);
+
     expect(result.body.error)
       .toContain('User validation failed: username: User name is to short');
 
@@ -68,6 +72,7 @@ describe('POST', () => {
       .send(newUser)
       .expect(400)
       .expect('Content-Type', /application\/json/);
+
     expect(result.body.error)
       .toContain('User validation failed: password: password is to short');
 
