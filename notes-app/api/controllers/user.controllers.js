583ac11b2917f9db708a9bba4559355556945040
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-10 16:01:04 +0100

    feat: Add getUser controller

---
+++ notes-app/api/controllers/user.controllers.js
@@ -0,0 +1,11 @@
+import User from '../models/user.models.js';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getUsers = async (req, res, next) => {
+  try {
+    const usersFound = await User.find();
+    Response.status(200).json(usersFound);
+  } catch (e) {
+    next(e);
+  }
+};
