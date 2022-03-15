c8c9ee94f23bb548d836f3647cf6d176feddb1b1
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-15 19:45:37 +0100

    fix: now throw proper error when logins fails

--- notes-app/api/controllers/login.controllers.js
+++ notes-app/api/controllers/login.controllers.js
@@ -3,32 +3,32 @@ import bcrypt from 'bcrypt';
 
 import User from '../models/user.models.js';
 
-export const getLogin = async (request, response) => {
+const isValidPassword = async (user, password) => (user === null
+  ? false
+  : bcrypt.compare(password, user.password));
+
+export const getLogin = async (request, response, next) => {
   const { body } = request;
   const { username, password } = body;
 
-  const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
+    const passwordIsCorrect = await isValidPassword(user, password);
+    if (!passwordIsCorrect) {
+      const errorMessage = { name: 'InvalidLogin', message: 'invalid user or password' };
+      throw errorMessage;
+    }
 
-  const passwordIsCorrect = user === null
-    ? false
-    : await bcrypt.compare(password, user.password);
+    const token = jwt.sign({ userName: user.username, user: user.id }, process.env.SECRET);
 
-  if (!(user && passwordIsCorrect)) {
-    return response.status(401).json({ error: 'invalid user or password' });
+    response.send({
+      name: user.name,
+      userName: user.userName,
+      token,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const userForToken = {
-    userName: user.username,
-    user: user.id,
-  };
-
-  const token = jwt.sign(userForToken, process.env.SECRET);
-
-  return response.send({
-    name: user.name,
-    userName: user.userName,
-    token,
-  });
 };
 
 export default getLogin;
