175120854c46b36f35181ee8c94932902dee8544
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-15 17:06:24 +0100

    test: add post notes tests

--- notes-app/api/tests/note.test.js
+++ notes-app/api/tests/note.test.js
@@ -18,6 +18,50 @@ describe('GET', () => {
     expect(response.body).toHaveLength(initialNotes.length);
   });
 });
+
+describe('POST', () => {
+  test('POST a valid note', async () => {
+    const newNote = { content: 'A valid Note', important: true };
+
+    await api.post('/api/notes')
+      .send(newNote)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const { response, contents } = await getAllNotes();
+    expect(response.body).toHaveLength(initialNotes.length + 1);
+    expect(contents).toContain(newNote.content);
+  });
+
+  test('POST fails with a proper status code and message if content is missing', async () => {
+    const newNote = { important: true };
+
+    const result = await api.post('/api/notes')
+      .send(newNote)
+      .expect(400);
+
+    expect(result.body.error)
+      .toContain('User validation failed: content: content is missing');
+
+    const { response } = await getAllNotes();
+    expect(response.body).toHaveLength(initialNotes.length);
+  });
+
+  test('POST fails with a proper status code and message if important is missing', async () => {
+    const newNote = { content: 'Invalid note without important' };
+
+    const result = await api.post('/api/notes')
+      .send(newNote)
+      .expect(400);
+
+    expect(result.body.error)
+      .toContain('User validation failed: important: iimportant is missing');
+
+    const { response, contents } = await getAllNotes();
+    expect(response.body).toHaveLength(initialNotes.length);
+    expect(contents).not.toContain(newNote.content);
+  });
+});
 afterAll(() => {
   mongoose.connection.close();
   closeServer();
