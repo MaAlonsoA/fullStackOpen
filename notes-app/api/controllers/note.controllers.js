d2cc0e3926a5109b651c1d2c5bf1b67a1a8d5535
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-16 17:42:37 +0100

    feat: add put controller for notes

--- notes-app/api/controllers/note.controllers.js
+++ notes-app/api/controllers/note.controllers.js
@@ -1,7 +1,6 @@
 import Note from '../models/note.models.js';
 import User from '../models/user.models.js';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getNotes = async (req, res, next) => {
   try {
     const notesFound = await Note.find({});
@@ -29,5 +28,38 @@ export const postNote = async (req, res, next) => {
     res.status(200).json(savedNote);
   } catch (e) {
     next(e);
+  }
+};
+
+export const deleteNote = async (req, res, next) => {
+  const { id } = req.params;
+  const { userId } = req;
+
+  try {
+    const noteToDelete = await Note.findById(id).orFail();
+    if (userId === noteToDelete.user.toString()) {
+      await Note.findByIdAndDelete(id);
+      res.status(200).end();
+    } else {
+      res.status(401).end();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateNote = async (req, res, next) => {
+  const { id } = req.params;
+  const { userId, body } = req;
+  const { content, important } = body;
+
+  try {
+    const noteToUpdate = await Note.findById(id).orFail();
+    if (userId === noteToUpdate.user.toString()) {
+      const updatedNote = await Note.findByIdAndUpdate(id, { content, important }, { new: true });
+      res.status(200).json(updatedNote);
+    }
+  } catch (error) {
+    next(error);
   }
 };
--- notes-app/api/routes/note.routes.js
+++ notes-app/api/routes/note.routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
-import { getNotes, postNote } from '../controllers/note.controllers.js';
+import {
+  deleteNote, getNotes, postNote, updateNote,
+} from '../controllers/note.controllers.js';
 import userExtractor from '../utils/middlewares/userExtractor.js';
 
 const router = Router();
 
 router.get('/', getNotes);
 router.post('/', userExtractor, postNote);
+router.delete('/:id', userExtractor, deleteNote);
+router.put('/:id', userExtractor, updateNote);
 
 export default router;
