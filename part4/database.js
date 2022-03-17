fef60fda343598dc8c8577ca37e3169ab826927b
Marcos Alonso <ma_lonso94@hotmail.com>
2022-03-17 13:05:49 +0100

    refactor: database connect now to a independet collection

--- part4/database.js
+++ part4/database.js
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
-import { MONGODB_URI } from './utils/config.js';
+import {
+  MONGO_DB_URI, MONGO_DB_URI_TEST, MONGO_DB_URI_DEV, NODE_ENV,
+} from './utils/config.js';
 import * as logger from './utils/logger.js';
 
-mongoose.connect(MONGODB_URI)
-  .then((result) => {
+let connectionString;
+
+if (NODE_ENV === 'production') {
+  connectionString = MONGO_DB_URI;
+} else if (NODE_ENV === 'development') {
+  connectionString = MONGO_DB_URI_DEV;
+} else if (NODE_ENV === 'test') {
+  connectionString = MONGO_DB_URI_TEST;
+}
+const connectDB = async (next) => {
+  try {
+    const result = await mongoose.connect(connectionString);
     logger.info('connected to MongoDB', result.connections[0].name);
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message);
-  });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default connectDB;
--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,5 @@
 import 'dotenv/config';
 
-export const { PORT } = process.env || 3001;
-// eslint-disable-next-line import/no-mutable-exports
-export let { MONGODB_URI } = process.env;
-
-if (process.env.NODE_ENV === 'test') {
-  MONGODB_URI = process.env.TEST_MONGODB_URI;
-}
+export const {
+  MONGO_DB_URI, MONGO_DB_URI_TEST, MONGO_DB_URI_DEV, NODE_ENV, PORT,
+} = process.env;
